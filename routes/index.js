a4eea014a977059a6f919342b28ad16a54c4ea4c
skw622 <skw622vlato@gmail.com>
2021-01-19 06:24:44 -0500

    backend

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var mainController = require('../controllers/main.controller')
+var main = new mainController();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/test', main.index);
+
+module.exports = router;
